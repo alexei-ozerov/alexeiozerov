c1de2d063c99120dc9260cc70a5ab6d6cd7ce9df
Alexei Ozerov <alexei.ozerov.7@gmail.com>
2024-09-13 20:17:37 -0400

    BUGFIX: resolve build issue

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import * as motion from "framer-motion/client"
 import FooterContent from "@/components/footer";
-import Animation from "@/components/animation";
 
 export default function Page() {
     return (
